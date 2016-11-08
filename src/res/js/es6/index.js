47438de6f01fff1b5b2bccfece197b76bb4883eb
lixinglong <804160646@qq.com>
2016-11-08 09:40:55 +0800

    '自定义滚动'

--- src/res/js/es6/index.js
+++ src/res/js/es6/index.js
@@ -1,5 +1,18 @@
 'use strict';
 ~function(){
-	const n = 2;
-	console.log(n);
+	function move({x =0,y=0} = {}){
+		console.log(x,y);
+	}
+	move({x:3,y:10})
+	move({x:4});
+	move({});
+	move();
+
+	function add({x,y}={x:0,y:0}){
+		console.log(x,y)
+	}
+	add({x:2,y:4});
+	add({x:2});
+	add({});
+	add();
 }()
