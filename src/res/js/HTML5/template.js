b00530990d5e26d002d2190742883e18b2780dd5
lixinglong <804160646@qq.com>
2017-03-10 16:13:30 +0800

    '手写模板引擎'

--- src/res/js/HTML5/template.js
+++ src/res/js/HTML5/template.js
@@ -16,29 +16,29 @@
 	//data: 数据对象
 	var template = function(text,data){
 		var index = 0;//记录当前扫描到哪里了
-		var function_body = "var temp = '';";
-		function_body += "temp += '";
-		text.replace(matcher,function(match,interpolate,evaluate,offset){
+		var fn_bd = "var temp = '';";
+		fn_bd += "temp += '";
+		text.replace(matcher,function(match,inter,eval,offset){
 			//找到第一个匹配后，将前面部分作为普通字符串拼接的表达式
 			//添加了处理转义字符
-			function_body += text.slice(index,offset)
+			fn_bd += text.slice(index,offset)
 				.replace(escaper, function(match) { return '\\' + escapes[match]; });
 
-			//如果是<% ... %>直接作为代码片段，evaluate就是捕获的分组
-			if(evaluate){
-				function_body += "';" + evaluate + "temp += '";
+			//如果是<% ... %>直接作为代码片段，eval就是捕获的分组
+			if(eval){
+				fn_bd += "';" + eval + "temp += '";
 			}
-			//如果是<%= ... %>拼接字符串，interpolate就是捕获的分组
-			if(interpolate){
-				function_body += "' + " + interpolate + " + '";
+			//如果是<%= ... %>拼接字符串，inter就是捕获的分组
+			if(inter){
+				fn_bd += "' + " + inter + " + '";
 			}
-			//递增index，跳过evaluate或者interpolate
+			//递增index，跳过eval或者inter
 			index = offset + match.length;
-			//这里的return没有什么意义，因为关键不是替换text，而是构建function_body
+			//这里的return没有什么意义，因为关键不是替换text，而是构建fn_bd
 			return match;
 		});
 		//最后的代码应该是返回temp
-		function_body += "';return temp;";
-		var render = new Function('obj', function_body);
+		fn_bd += "';return temp;";
+		var render = new Function('obj', fn_bd);
 		return render(data);
 	}
