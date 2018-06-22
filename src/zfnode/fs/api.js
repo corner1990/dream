9b20dd3d2db4a5839df0e1a6c78d2ceee97c7ee1
corner1990 <804160646@qq.com>
2018-06-22 23:40:34 +0800

    添加异步删除学习代码

--- src/zfnode/fs/api.js
+++ src/zfnode/fs/api.js
@@ -197,3 +197,100 @@ function rmPromise (dir) {
     })
 }
 rmPromise('a')
+
+
+const fs = require('fs')
+const path = require('path')
+
+/**
+ * 
+ * @desc 异步深度循环删除目录
+ * @param {string} dir 需要删除的目录 
+ * @param {function} callback 回调函数
+ * 
+ * 实现思路： 
+ * 1.读取文件目录拿到当前目录所有的files
+ * 2.调用next方法，并从0开始遍历files
+ * 3.遍历结束，调用callbanck
+ */
+function rmdir (dir, callback) {
+fs.readdir(dir, (err, files) => {
+    /**
+     * @desc 内部循环遍历使用的工具函数
+     * @param {Number} index 表示读取files的下标 
+     */
+    function next(index) {
+        // 如果index 等于当前files的时候说明循环遍历已经完毕，可以删除dir，并且调用callback
+        if (index == files.length) return fs.rmdir(dir, callback)
+        // 如果文件还没有遍历结束的话，继续拼接新路径，使用fs.stat读取该路径
+        let newPath = path.join(dir, files[index])
+        // 读取文件，判断是文件还是文件目录
+        
+        fs.stat(newPath, (err, stat) => {
+            if (stat.isDirectory() ) {
+                // 因为我们这里是深度循环，也就是说遍历玩files[index]的目录以后，才会去遍历files[index+1]
+                // 所以在这里直接继续调用rmdir，然后把循环下一个文件的调用放在当前调用的callback中
+                rmdir(newPath, () => next(index+1))
+            } else {
+                // 如果是文件，则直接删除该文件，然后在回调函数中调用遍历nextf方法，并且index+1传进去
+                fs.unlink(newPath, () => next(index+1))
+            }
+        })
+    }
+    next(0)
+})
+}
+
+ rmdir('a', () => {console.log('文件删除完毕')})
+
+
+/**
+ * 广度遍历删除文件
+ */
+const fs = require('fs')
+const path = require('path')
+
+// 同步删除
+
+/**
+ * @desc 同步广度删除
+ * @param {string} dir 需要删除的目录
+ * 
+ * 实现思路
+ * 1.创建需要的变量，arr=> 保存所有的路径的文件池 current => 当前遍历到的路径 index => 记录比遍历的下标
+ * 2.使用while循环，拿到所有的路径
+ * 3.使用fs.stat判断，如果当前路径是一个目录，使用fs.readdirSync 读取所有文件内容，
+ * 4.使用map函数映射files路经(拼接当前遍历到的carrent和file),
+ * 5.添加到文件池中去
+ */
+function rmdir(dir) {
+    let arr = [dir]
+    let current = null
+    let index = 0
+
+    while(current = arr[index++]) {
+        // 读取当前文件，并做一个判断，文件目录分别处理
+        let stat = fs.statSync(current)
+        //如果文件是目录
+        if (stat.isDirectory()) {
+            //读取当前目录，拿到所有文件
+            let files = fs.readdirSync(current)
+            // 将文件添加到文件池
+            arr = [...arr, ...files.map(file => path.join(current, file))]
+        }
+    }
+    //遍历删除文件
+    for (var i = arr.length - 1; i >= 0; i--) {
+        // 读取当前文件，并做一个判断，文件目录分别处理
+        let stat = fs.statSync(arr[i])
+        // 目录和文件的删除方法不同
+        if (stat.isDirectory()) {
+            fs.rmdirSync(arr[i])
+        } else {
+            fs.unlinkSync(arr[i])
+        }
+    }
+}
+
+rmdir('a')
+
