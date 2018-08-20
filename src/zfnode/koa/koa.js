c3ca15c08874e77e5e601f53f544f0419211a94a
corner1990 <804160646@qq.com>
2018-08-21 07:09:10 +0800

    保存koa笔记

--- src/zfnode/koa/koa.js
+++ src/zfnode/koa/koa.js
@@ -1,18 +1,33 @@
-// let Koa = require('koa')
-let Koa = require('./koa/application')
+let Koa = require('koa')
+// let Koa = require('./koa/application')
 // koa 是一个类，有两个方法，一个叫做use 一个叫做listen
 let app = new Koa()
-app.listen(5000)
+app.listen(3000)
 
-// ctx 代表的是上下文，是koa自己创建的
-// ctx.request 这是koa自己封装的request属性
-// ctx.response
-// ctx.req 原生req对象
-// ctx.res
-app.use((ctx, next) => {
-    ctx.response.body = '100'
-    // ctx.body 可以设置多次，但是会以最后一个为结果
-    console.log(ctx.response.body)
-    // console.log(ctx.path)
-    // ctx.body = '1231231'
+// 第一个中间件中调用了next，走到了第二个中间件
+// 第二个中间件又等待效果，第二个处于等待状态，那就等着，是第一个会接着走
+// 只要调用next方法就加上await，防止下一个中间件又异步操作
+// koa中如果不需要继续，可以直接return next
+app.use(async (ctx, next) => {
+    console.log(1)
+    await log()
+    next()
+    console.log(2)
 })
+app.use((ctx, next) => {
+    console.log(3)
+    next()
+    console.log(4)
+})
+app.use((ctx, next) => {
+    ctx.body = 'ok'
+    console.log(5)
+    next()
+    console.log(6)
+})
+
+function log () {
+    setTimeout(function (params) {
+        console.log('log')
+    }, 3000)
+}
--- src/zfnode/proxy/vertiula.js
+++ src/zfnode/proxy/vertiula.js
@@ -1,6 +1,7 @@
 // 反向代理
 let httpProxy = require('http-proxy')
 let http = require('http');
+// 这是我们配置的域名，我们可以访问这些域名，拿到对应的结果
 let hosts = {
   'www.test.com': 'http://localhost:5000',
   'www.demo.com': 'http://localhost:8000',
@@ -12,8 +13,6 @@ let proxy = httpProxy.createProxyServer()
 let server = http.createServer((req, res) => {
   // 拿到host 访问对应的服务器
   let host = req.headers['host'].split(':')[0]
-  console.log(host)
-  console.log(hosts[host])
   proxy.web(req, res, {
     target: hosts[host]
   })
