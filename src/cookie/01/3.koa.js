ddb24463eac33e6d6f0239c6d4421c5c41be2ba5
corner <804160646@qq.com>
2018-10-24 20:11:42 +0800

    学习session

--- src/cookie/01/3.koa.js
+++ src/cookie/01/3.koa.js
@@ -4,56 +4,44 @@ let Router = require('koa-router')
 
 let router = new Router()
 let app = new Koa()
-
-app.use(async (ctx, next) => {
-    ctx.getCookie = (key) => {
-       let r =  ctx.get('Cookie') || ''
-       let cookieObj = require('querystring').parse(r, '; ')
-       return cookieObj[key]
-    }
-    let allCookies = []
-    ctx.setCookie = (key, val, opts)  => {
-        let arr = []
-        let line = `${key}=${encodeURIComponent(val)}`
-        arr.push(line)
-        
-        if(opts) {
-            if (opts.domain) {
-                arr.push(`Domain=${opts.domain}`)
-            }
-    
-            if (opts.maxAge) {
-                arr.push(`Max-Age=${opts.maxAge}`)
-            }
-    
-            if (opts.path) {
-                arr.push(`Path=${opts.path}`)
-            }
-    
-            if (opts.httpOnly) {
-                arr.push(`HttpOnly=${opts.httpOnly}`)
-            }
-        }
-
-        line = arr.join('; ')
-        allCookies.push(line)
-        ctx.set('Set-Cookie', allCookies)
-    }
-    await next()
-})
-
+app.keys = ['im a newer secret', 'i like turtle'];
 router.get('/read', (ctx, next) => {
     let name = ctx.cookies.get('name') || 'name is not found'
     let age = ctx.cookies.get('age') || 'age is not found'
-    ctx.body = age
+    ctx.body = 'age'+ age + 'name=' + name
 })
-router.get('/write', (ctx, next) => {
-    ctx.cookies.set('name', 'hello', {domain: 'localhost'})
-    ctx.cookies.set('age', 18, {maxAge: 1000})
-    ctx.cookies.set('address', 'hahhhhhh')
-    ctx.body = 'write ok'
+// router.get('/visit', (ctx, next) => {
+//     let visit = ctx.cookies.get('visit')
+//     visit = visit ? (parseInt(visit) + 1) : 1
+//     ctx.cookies.set('visit', visit)
+//     // 添加签名
+//     ctx.cookies.set('visit', visit, { signed: true })
+
+//     // 签名 2(第二次访问)
+
+//     // 3 (第三次访问)
+//     ctx.body = `当前用户第 ${visit} 访问`
+
+// })
+
+// 签名效验 防止篡改 cookie； 虽然看起来安全了很多，但还是不能够放敏感信息
+router.get('/visit', (ctx, next) => {
+    let visit = ctx.cookies.get('visit', { signed: true })
+    visit = visit ? (parseInt(visit) + 1) : 1
+    ctx.cookies.set('visit', visit)
+    // 添加签名
+    ctx.cookies.set('visit', visit, { signed: true })
+
+    // 签名 2(第二次访问)
+
+    // 3 (第三次访问)
+    ctx.body = `当前用户第 ${visit} 访问`
 
 })
 
 app.use(router.routes())
 app.listen(4000)
+
+// let crypto = require('crypto')
+// let r = crypto.createHmac('sha1', 'hello').update(String(1)).digest('base64')
+// console.log(r) // VswpfHypMb5p/KgpBL3Dm6OQ4Kk=
