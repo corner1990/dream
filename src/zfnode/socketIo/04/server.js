bc9ac627fd610b07a894bd3ba6ecafbcd32084d3
corner <804160646@qq.com>
2018-11-21 20:39:04 +0800

    学习mongodb

--- src/zfnode/socketIo/04/server.js
+++ src/zfnode/socketIo/04/server.js
@@ -13,31 +13,32 @@ let server = http.createServer(app)
 let socketIo = require('socket.io')
 let io = socketIo(server)
 // 在服务器监听客户端的链接
-let users = [] // 保存用户
+let sockets = {} // 保存用户
 let SYS = '系统提示'
 let t = new Date()
 
 io.on('connection', socket => {
     console.log('客户端连接到服务器')
     // 监听接受客户端发过来的消息
+    let username;
     socket.on('message', msg => {
-        if (!socket.name) {
-            socket.name = msg
+        if (!username) {
+            username = msg
             let message = {
                 name: SYS,
                 timer: t.getTime(),
                 msg: `${msg} 进入聊天`
             }
-            users.push(socket)
+           sockets[username] = socket
             socket.broadcast.emit('message', JSON.stringify(message))
         } else {
             // 像客户端发送数据
             // 解析数据，看是否又私密聊天
             if (msg.match(/^@([^ ]+) (\S)+/)) {
                 let [, name, m] = msg.match(/^@([^ ]+) (\S)+/)
-                let so = users.filter(item => item.name === name)[0]
-                if (so) {
-                    if (so.name === socket.name) {
+    
+                if (name) {
+                    if (name === username) {
                         let message = {
                             name: SYS,
                             timer: t.getTime(),
@@ -50,7 +51,7 @@ io.on('connection', socket => {
                             timer: t.getTime(),
                             msg: m
                         }
-                        so.emit('message', JSON.stringify(message))
+                        sockets[username].emit('message', JSON.stringify(message))
                     }
                     
                 } else { // 如果不存在，则提示用户不存在
