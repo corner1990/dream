9a6867485ec0b1be590b0206141bb9e6e9ede5bc
corner <804160646@qq.com>
2018-11-24 17:30:28 +0800

    学习mongodb

--- src/zfnode/mongoose/3.js
+++ src/zfnode/mongoose/3.js
@@ -7,30 +7,55 @@ let mongoose = require('mongoose')
  * @param {Object} schema 可以定义的schema， 这里没有做过多的处理，自己可以处理批量或者直接返回
  * @param {String} doc 集合的名称
  */
-const createTable = (url, name, schema, doc ) => {
-  let dbUrl = `${url}/${name}`
+const createTable = async (url, name, schema, doc ) => {
+  let dbUrl = `mongodb://root2:123456@${url}/school`
+ console.log('dbUrl', dbUrl)
   const Schema = mongoose.Schema;
-  let con = mongoose.createConnection(dbUrl, { useNewUrlParser: true })
+  return mongoose.createConnection(dbUrl, { useNewUrlParser: true })
+                    .then(function(res) {
+                        // let temp = new Schema(schema)
 
-  // 定义骨架模型
-  let temp = new Schema(schema)
+                        // 定义数据库操作
+                        // let model = res.model(doc, temp)
 
-  // 定义数据库操作
-  let model = con.model(doc, temp)
-
-  return model
+                        // return model
+                        res.createUser({
+                            name: 'test',
+                            pwd: 123456,
+                            roles:[
+                                {
+                                    role:"readWrite",
+                                    db:"school"
+                                },
+                                'read'
+                           ]
+                        })
+                    }, function (err) {
+                        console.log('err', err)
+                    })
+//   const conn = mongoose.createConnection('mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]', options);
 }
 
 /**
  * 使用
  */
-let test = createTable('mongodb://localhost:27017', 'hello', {
+let test = createTable('localhost:27017', 'hello', {
   name: String,
   age: Number
 }, 'testCreate')
+// .then(function (mode) {
+//     console.log('mode', mode)
+//     // mode.create({name: 'textCreate', age: 10})
+//     // .then(
+//     //   res => {console.log('res', res)},
+//     //   err => {console.log('err', err)}
+//     // )
+// })
 
-test.create({name: 'textCreate', age: 10})
-  .then(
-    res => {console.log('res', res)},
-    err => {console.log('err', err)}
-  )
+// test.create({name: 'textCreate', age: 10})
+//   .then(
+//     res => {console.log('res', res)},
+//     err => {console.log('err', err)}
+//   )
+
+
