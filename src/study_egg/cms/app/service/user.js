0e5d73564dff4647f653839d9a3ec159c7c35d7c
corner <804160646@qq.com>
2018-12-08 15:03:49 +0800

    学习redis

--- src/study_egg/cms/app/service/user.js
+++ src/study_egg/cms/app/service/user.js
@@ -4,8 +4,8 @@ module.exports = app => {
   return class UserService extends app.Service {
     async create(user) {
       const { app } = this;
-      console.log('app.redis.', user, app.redis.hset);
-      const res = await app.redis.hset('name', 'hname', 'age', '18');
+      console.log('app.redis.', user);
+      const res = await app.redis.hgetall('mm');
       return res;
     }
   };
