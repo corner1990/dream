a2fcde141565852d40ff841e7ba0d3f4fa99321f
lixinglong <lixinglong1@globalegrow.com>
2019-05-06 18:32:56 +0800

    学习react-ssr

--- react_ssr/src/containers/Counter/index.js
+++ react_ssr/src/containers/Counter/index.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux"
 import actions from '../../store/actions/counter'
 
 class Counter extends React.Component{
-
+    state = {number: 0}
     render () {
         return (<div style={{textAlign: 'center'}}>
             <p>{this.props.number}</p>
@@ -16,4 +16,6 @@ const WrapCounter = connect(
     state => state.counter,
     actions
 )(Counter)
+// Counter = connect(state => state.counter, actions)(Counter)
 export default WrapCounter
+// export default Counter
