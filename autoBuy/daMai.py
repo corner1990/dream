a33b51909454c10d1f74d2ca778c67bedf1a4990
lixinglong <lixl@authine.com>
2019-12-17 14:11:44 +0800

    处理脚本

--- autoBuy/daMai.py
+++ autoBuy/daMai.py
@@ -2,19 +2,37 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ChromeOptions
 import time
 import datetime
+from user_info import userInfo
+
   
-  
+def inputInfo(browser):
+  time.sleep(5);
+  user = userInfo[0];
+  try:
+    name = "document.querySelector('#fm-login-id').value=" + str(user.get('account'))
+    print('name => %s' % name)
+    # name.send_keys(user.get('account'));
+    browser.execute_async_script(name)
+    # password.send_keys(user.get('password'));
+    print('input')
+  except:
+    print('input throw error')
+    pass;
+
+
 def login():
   # 打开淘宝登录页，并进行扫码登录
   browser.get("https://www.damai.cn/")
-
   time.sleep(2)
+  browser.maximize_window()
   browser.find_element_by_class_name('span-user').click()
-  time.sleep(3)
-  browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[3]').click()
-  time.sleep(12)
+  
+  inputInfo(browser)
+  # browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[3]').click()
+  time.sleep(1200)
   browser.get('https://detail.damai.cn/item.htm?id=609530171491');
   now = datetime.datetime.now()
   print('login success:', now.strftime('%Y-%m-%d %H:%M:%S'))
@@ -23,11 +41,13 @@ def login():
   buy(times)
 
 def buy(times):
+  isSelscted = True;
   while True:
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
     if now > times:
       # 点击结算按钮
       while True:
+        
         try:
           if browser.find_element_by_class_name('buybtn'):
             browser.find_element_by_class_name('buybtn').click()
@@ -36,15 +56,17 @@ def buy(times):
         
         except:
           print("立即预定")
+        if isSelscted:
+          try:
+              if browser.find_element_by_class_name('next-checkbox-label'):
+                  browser.find_element_by_class_name('next-checkbox-label').click()
+                  now1 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+          except:
+              print("选择观影人")
+          isSelscted = False;
         try:
-            if browser.find_element_by_class_name('next-checkbox-label'):
-                browser.find_element_by_class_name('next-checkbox-label').click()
-                now1 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-        except:
-            print("选择观影人")
-        try:
-            if browser.find_element_by_class_name('next-btn'):
-                browser.find_element_by_class_name('next-btn').click()
+            if browser.find_element_by_css_selector('.submit-wrapper .next-btn'):
+                browser.find_element_by_css_selector('.submit-wrapper .next-btn').click()
                 now1 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
         except:
             print("重新下单")
@@ -55,6 +77,9 @@ if __name__ == "__main__":
   # pages = int(input('跳转值商品页面请输入“1”， 跳转到购车从，请输入任意数字:'));
   # 时间格式："2019-12-12 18:18:00.000000"
   # times = int('2019-12-16 18:31:58.000000');
-  browser = webdriver.Chrome()
+  option = ChromeOptions()
+  option.add_experimental_option('excludeSwitches', ['enable-automation'])
+  browser = webdriver.Chrome(options = option)
   # browser.maximize_window()
+  
   login()
---
+++ autoBuy/user_info.py
@@ -0,0 +1,8 @@
+userInfo = [
+    {
+        "account": 15276513522,
+        "password": "que90.11.25"
+    }
+]
+
+# print(userInfo[0].get('account'))
